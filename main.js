08c9fccde2ecce33a3e5494b61b3684961b69676
NedaNar <neda.narmontaite@ktu.edu>
2024-01-04 12:30:47 +0000

    works

--- main.js
+++ main.js
@@ -13,8 +13,26 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.BasicShadowMap;
 document.body.appendChild(renderer.domElement);
 
+// -------------------- OBJECT SETUP ---------------------- //
+function createObject(
+  geometry,
+  position,
+  parent,
+  material = pigMaterial,
+  rotation = { x: 0, y: 0, z: 0 }
+) {
+  const object = new THREE.Mesh(geometry, material);
+  object.position.set(position.x, position.y, position.z);
+  object.rotation.set(rotation.x, rotation.y, rotation.z);
+  object.castShadow = true;
+  object.receiveShadow = true;
+  parent.add(object);
+  return object;
+}
+
 const textureLoader = new THREE.TextureLoader();
 
+// BACKGROUND
 const forestTexture = textureLoader.load("images/bg.png");
 var forestGeometry = new THREE.PlaneGeometry(
   window.innerWidth * 0.36,
@@ -27,8 +45,6 @@ var forestMaterial = new THREE.MeshPhongMaterial({
   side: THREE.DoubleSide,
 });
 var forest = new THREE.Mesh(forestGeometry, forestMaterial);
-forest.receiveShadow = true;
-
 forest.position.y = 15;
 forest.position.z = -99;
 scene.add(forest);
@@ -45,228 +61,177 @@ var lakeMaterial = new THREE.MeshPhongMaterial({
   side: THREE.DoubleSide,
 });
 lakeMaterial.receiveShadow = true;
-
 var lake = new THREE.Mesh(lakeGeometry, lakeMaterial);
 lake.receiveShadow = true;
-lake.rotation.x = Math.PI / 2 *3;
+lake.rotation.x = (Math.PI / 2) * 3;
 lake.position.y = 15;
 lake.position.z = -110;
 scene.add(lake);
 
 // STARS
 var starGeom = new THREE.PlaneGeometry(65, 50);
-const starTexture = textureLoader.load("images/star.png");
 var starMaterial = new THREE.MeshPhongMaterial({
-  map: starTexture,
+  map: textureLoader.load("images/star.png"),
   side: THREE.DoubleSide,
   transparent: true,
   alphaTest: 0.5,
 });
-var star1 = new THREE.Mesh(starGeom, starMaterial);
-star1.position.x = -180;
-star1.position.z = -65;
-star1.position.y = 90;
-star1.castShadow = true;
-scene.add(star1);
-
-var star2 = new THREE.Mesh(starGeom, starMaterial);
-star2.position.x = -15;
-star2.position.z = -40;
-star2.position.y = 55;
-star2.castShadow = true;
-scene.add(star2);
-
-var star3 = new THREE.Mesh(starGeom, starMaterial);
-star3.position.x = 190;
-star3.position.z = -80;
-star3.position.y = 110;
-star3.castShadow = true;
-scene.add(star3);
+createObject(starGeom, { x: -185, y: 60, z: -65 }, scene, starMaterial);
+createObject(starGeom, { x: -15, y: 65, z: -40 }, scene, starMaterial);
+createObject(starGeom, { x: 200, y: 85, z: -80 }, scene, starMaterial);
 
 // DOORS
-var door1Geom = new THREE.PlaneGeometry(32, 40);
-const door1Texture = textureLoader.load("images/door1.png");
-var door1Material = new THREE.MeshPhongMaterial({
-  map: door1Texture,
-  side: THREE.DoubleSide,
-});
-var door1 = new THREE.Mesh(door1Geom, door1Material);
-door1.position.x = -145;
-door1.position.z = -99;
-door1.position.y = 38;
-scene.add(door1);
-
-var door4Geom = new THREE.PlaneGeometry(32, 32);
-const door4Texture = textureLoader.load("images/door4.png");
-var door4Material = new THREE.MeshPhongMaterial({
-  map: door4Texture,
-  side: THREE.DoubleSide,
-});
-var door4 = new THREE.Mesh(door4Geom, door4Material);
-door4.position.x = 143;
-door4.position.z = -99;
-door4.position.y = 35;
-scene.add(door4);
+const door1 = createObject(
+  new THREE.PlaneGeometry(32, 40),
+  { x: -145, y: 38, z: -98 },
+  scene,
+  new THREE.MeshPhongMaterial({
+    map: textureLoader.load("images/door1.png"),
+    side: THREE.DoubleSide,
+  })
+);
+const door4 = createObject(
+  new THREE.PlaneGeometry(32, 32),
+  { x: 143, y: 35, z: -98 },
+  scene,
+  new THREE.MeshPhongMaterial({
+    map: textureLoader.load("images/door4.png"),
+    side: THREE.DoubleSide,
+  })
+);
+const doorActions = [
+  { action: "restart", door: door1 },
+  { action: "run", door: door4 },
+];
 
 // PIG
-const pigGeometry = new THREE.BoxGeometry(7, 5, 4);
-const pigMaterial = new THREE.MeshStandardMaterial({ color: "#FFC0CB" });
-const pig = new THREE.Mesh(pigGeometry, pigMaterial);
-pig.castShadow = true;
-pig.receiveShadow = true;
-pig.position.set(0, 0, 0);
-pig.rotation.y = Math.PI / 8;
-scene.add(pig);
-
-const headGeometry = new THREE.BoxGeometry(4.5, 4.5, 4.5);
-const head = new THREE.Mesh(headGeometry, pigMaterial);
-head.castShadow = true;
-head.receiveShadow = true;
-head.position.set(-2, 2, 0);
-pig.add(head);
-
-const headImg = textureLoader.load("images/face.png");
-const headMaterial = new THREE.MeshBasicMaterial({
-  map: headImg,
-  transparent: true,
+const pigMaterial = new THREE.MeshPhongMaterial({
+  color: "#FFC0CB",
+  flatShading: true,
 });
-const headFace = new THREE.Mesh(
+const pig = createObject(
+  new THREE.BoxGeometry(7, 5, 4),
+  { x: 150, y: 20, z: -105 },
+  scene,
+  pigMaterial,
+  { x: 0, y: Math.PI / 12, z: 0 }
+);
+
+// HEAD
+const head = createObject(
+  new THREE.BoxGeometry(4.5, 4.5, 4.5),
+  { x: -2, y: 2, z: 0 },
+  pig
+);
+createObject(
   new THREE.PlaneGeometry(4.5, 4.5),
-  headMaterial
+  { x: -2.25, y: 0, z: 0 },
+  head,
+  new THREE.MeshBasicMaterial({
+    map: textureLoader.load("images/face.png"),
+    transparent: true,
+  }),
+  { x: 0, y: (Math.PI / 2) * 3, z: 0 }
 );
-headFace.position.x = -2.25;
-headFace.rotation.y = (Math.PI / 2) * 3;
-head.add(headFace);
 
-const mouthGeometry = new THREE.BoxGeometry(2.5, 1.5, 2);
-const mouth = new THREE.Mesh(mouthGeometry, pigMaterial);
-mouth.castShadow = true;
-mouth.receiveShadow = true;
-mouth.position.set(-1.8, -0.5, 0);
-head.add(mouth);
-
-const mouthImg = textureLoader.load("images/nose.png");
-const mouthMaterial = new THREE.MeshBasicMaterial({
-  map: mouthImg,
-  transparent: true,
-});
-const mouthFace = new THREE.Mesh(
+// MOUTH
+const mouth = createObject(
+  new THREE.BoxGeometry(2.5, 1.5, 2),
+  { x: -1.8, y: -0.5, z: 0 },
+  head
+);
+createObject(
   new THREE.PlaneGeometry(2.5, 1.5),
-  mouthMaterial
+  { x: -1.3, y: 0, z: 0 },
+  mouth,
+  new THREE.MeshBasicMaterial({
+    map: textureLoader.load("images/nose.png"),
+    transparent: true,
+  }),
+  { x: 0, y: (Math.PI / 2) * 3, z: 0 }
 );
-mouthFace.position.x = -1.3;
-mouthFace.rotation.y = (Math.PI / 2) * 3;
-mouth.add(mouthFace);
 
-const earGeometry = new THREE.BoxGeometry(0.5, 1.5, 2);
-const ear1 = new THREE.Mesh(earGeometry, pigMaterial);
-ear1.position.set(-1, 2.8, -1.2);
-ear1.castShadow = true;
-ear1.receiveShadow = true;
-head.add(ear1);
-const ear2 = new THREE.Mesh(earGeometry, pigMaterial);
-ear2.position.set(-1, 2.8, 1.2);
-ear2.castShadow = true;
-ear2.receiveShadow = true;
-head.add(ear2);
-
-const earImg = textureLoader.load("images/ear.png");
+// EARS
+const ear1 = createObject(
+  new THREE.BoxGeometry(0.5, 1.5, 2),
+  { x: -1, y: 2.8, z: -1.2 },
+  head
+);
+const ear2 = createObject(
+  new THREE.BoxGeometry(0.5, 1.5, 2),
+  { x: -1, y: 2.8, z: 1.2 },
+  head
+);
 const earMaterial = new THREE.MeshBasicMaterial({
-  map: earImg,
+  map: textureLoader.load("images/ear.png"),
   transparent: true,
 });
-const earFace1 = new THREE.Mesh(new THREE.PlaneGeometry(2.5, 1.5), earMaterial);
-earFace1.position.x = -0.3;
-earFace1.rotation.y = (Math.PI / 2) * 3;
-ear1.add(earFace1);
-const earFace2 = new THREE.Mesh(new THREE.PlaneGeometry(2.5, 1.5), earMaterial);
-earFace2.position.x = -0.3;
-earFace2.rotation.y = (Math.PI / 2) * 3;
-ear1.add(earFace2);
-ear2.add(earFace2);
+createObject(
+  new THREE.PlaneGeometry(2.5, 1.5),
+  { x: -0.3, y: 0, z: 0 },
+  ear1,
+  earMaterial,
+  { x: 0, y: (Math.PI / 2) * 3, z: 0 }
+);
+createObject(
+  new THREE.PlaneGeometry(2.5, 1.5),
+  { x: -0.3, y: 0, z: 0 },
+  ear2,
+  earMaterial,
+  { x: 0, y: (Math.PI / 2) * 3, z: 0 }
+);
+const eartop = createObject(
+  new THREE.BoxGeometry(1, 0.5, 2),
+  { x: -0.25, y: 0.5, z: 0 },
+  ear1
+);
+const eartop2 = createObject(
+  new THREE.BoxGeometry(1, 0.5, 2),
+  { x: -0.25, y: 0.5, z: 0 },
+  ear2
+);
 
-const eartopGeometry = new THREE.BoxGeometry(1, 0.5, 2);
-const eartop = new THREE.Mesh(eartopGeometry, pigMaterial);
-eartop.position.set(-0.25, 0.5, 0);
-eartop.castShadow = true;
-eartop.receiveShadow = true;
-const eartop2 = eartop.clone();
-ear1.add(eartop);
-ear2.add(eartop2);
+// TAIL
+const tail1 = createObject(
+  new THREE.BoxGeometry(0.5, 3, 0.5),
+  { x: 3, y: 3, z: 0 },
+  pig
+);
+const tail2 = createObject(
+  new THREE.BoxGeometry(1.5, 0.5, 0.5),
+  { x: -0.5, y: 1.5, z: 0 },
+  tail1
+);
+const tail3 = createObject(
+  new THREE.BoxGeometry(0.5, 1.5, 0.5),
+  { x: -0.5, y: -0.5, z: 0 },
+  tail2
+);
 
-const tailGeometry1 = new THREE.BoxGeometry(0.5, 3, 0.5);
-const tail1 = new THREE.Mesh(tailGeometry1, pigMaterial);
-tail1.position.set(3, 3, 0);
-tail1.castShadow = true;
-tail1.receiveShadow = true;
-pig.add(tail1);
-
-const tailGeometry2 = new THREE.BoxGeometry(1.5, 0.5, 0.5);
-const tail2 = new THREE.Mesh(tailGeometry2, pigMaterial);
-tail2.position.set(-0.5, 1.5, 0);
-tail2.castShadow = true;
-tail2.receiveShadow = true;
-tail1.add(tail2);
-
-const tailGeometry3 = new THREE.BoxGeometry(0.5, 1.5, 0.5);
-const tail3 = new THREE.Mesh(tailGeometry3, pigMaterial);
-tail3.position.set(-0.5, -0.5, 0);
-tail3.castShadow = true;
-tail3.receiveShadow = true;
-tail2.add(tail3);
-
+// LEGS
 const legGeometry = new THREE.BoxGeometry(2, 3, 1.9);
-const leg_br = new THREE.Mesh(legGeometry, pigMaterial);
-leg_br.position.set(2.2, -2, 2);
-leg_br.castShadow = true;
-leg_br.receiveShadow = true;
+const leg_br = createObject(legGeometry, { x: 2.2, y: -2, z: 2 }, pig);
 leg_br.rotation.z = Math.PI / 8;
-pig.add(leg_br);
-const leg_bl = new THREE.Mesh(legGeometry, pigMaterial);
-leg_bl.position.set(2.2, -2, -2);
-leg_bl.castShadow = true;
-leg_bl.receiveShadow = true;
+const leg_bl = createObject(legGeometry, { x: 2.2, y: -2, z: -2 }, pig);
 leg_bl.rotation.z = Math.PI / 8;
-pig.add(leg_bl);
-const leg_fl = new THREE.Mesh(legGeometry, pigMaterial);
-leg_fl.position.set(-2.2, -2, 2);
-leg_fl.castShadow = true;
-leg_fl.receiveShadow = true;
+const leg_fl = createObject(legGeometry, { x: -2.2, y: -2, z: 2 }, pig);
 leg_fl.rotation.z = -Math.PI / 8;
-pig.add(leg_fl);
-const leg_fr = new THREE.Mesh(legGeometry, pigMaterial);
-leg_fr.position.set(-2.2, -2, -2);
-leg_fr.castShadow = true;
-leg_fr.receiveShadow = true;
+const leg_fr = createObject(legGeometry, { x: -2.2, y: -2, z: -2 }, pig);
 leg_fr.rotation.z = -Math.PI / 8;
-pig.add(leg_fr);
 
-const bottomGeometry = new THREE.BoxGeometry(2, 0.5, 1.9);
+const bottomGeometry = new THREE.BoxGeometry(2.1, 0.6, 2);
 const bottomMaterial = new THREE.MeshStandardMaterial({ color: "#68411e" });
-const legBottom = new THREE.Mesh(bottomGeometry, bottomMaterial);
-legBottom.position.set(0, -1.5, 0);
-legBottom.castShadow = true;
-legBottom.receiveShadow = true;
-leg_br.add(legBottom);
-const legBottom2 = legBottom.clone();
-leg_bl.add(legBottom2);
-const legBottom3 = legBottom.clone();
-leg_fl.add(legBottom3);
-const legBottom4 = legBottom.clone();
-leg_fr.add(legBottom4);
-
-pig.position.set(150, 20, -105);
-pig.rotation.y = Math.PI / 12;
-
-camera.position.z = 20;
-camera.position.y = 20;
+createObject(bottomGeometry, { x: 0, y: -1.5, z: 0 }, leg_br, bottomMaterial);
+createObject(bottomGeometry, { x: 0, y: -1.5, z: 0 }, leg_bl, bottomMaterial);
+createObject(bottomGeometry, { x: 0, y: -1.5, z: 0 }, leg_fl, bottomMaterial);
+createObject(bottomGeometry, { x: 0, y: -1.5, z: 0 }, leg_fr, bottomMaterial);
 
 // LIGHTS
-const ambientLight = new THREE.AmbientLight("white", 0.9);
+const ambientLight = new THREE.AmbientLight("white", 1);
 scene.add(ambientLight);
 
-const pigLight = new THREE.DirectionalLight("orange", 0.5);
-pigLight.position.set(-120, 200, -40);
+const pigLight = new THREE.DirectionalLight("orange", 0.7);
+pigLight.position.set(-50, 100, 40);
 pigLight.castShadow = true;
 pigLight.shadow.camera.left = -20;
 pigLight.shadow.camera.right = 20;
@@ -275,6 +240,24 @@ pigLight.shadow.camera.bottom = -20;
 pigLight.target = pig;
 scene.add(pigLight);
 
+const light = new THREE.DirectionalLight("white", 0.5);
+light.position.set(0, 200, 0);
+light.target = lake;
+scene.add(light);
+
+const bgLight = new THREE.DirectionalLight("white", 1);
+bgLight.position.set(0, 100, 100);
+//bgLight.target = forest;
+scene.add(bgLight);
+
+var addlight = new THREE.DirectionalLight(0xffffff);
+addlight.position.set(0, 1, 1).normalize();
+scene.add(addlight);
+
+camera.position.z = 20;
+camera.position.y = 20;
+
+// -------------------- INITIAL VALUES ---------------------- //
 let initialPig = pig.position.y;
 let initialHead = head.position.y;
 let initialleg_br = leg_br.rotation.y;
@@ -289,39 +272,46 @@ let pause = false;
 let rotationSpeed = 0;
 let runningSpeed = 0.1;
 
+let shouldShowPig = true;
 let shouldAnimatePig = false;
 
+// -------------------- ANIMATION FUNCTIONCS ---------------------- //
 var animate = function () {
   requestAnimationFrame(animate);
-  if (shouldAnimatePig) {
-    animatePig();
-  }
+  shouldShowPig ? showPig() : shouldAnimatePig && animatePig();
   renderer.render(scene, camera);
 };
 
 function animatePig() {
   if (pig.position.z < 15) {
-    if (hasSlipped) runningSpeed -= 0.0005;
+    if (hasSlipped) runningSpeed -= 0.0002;
     else runningSpeed += 0.0003;
 
-    pig.position.x -= runningSpeed * 1.4;
+    if (doorActions[1].action == "run") {
+      pig.position.x -= runningSpeed * 1.4;
+    } else {
+      pig.position.x += runningSpeed * 1.4;
+    }
     pig.position.z += runningSpeed;
   } else {
     showGift();
     resumeAnimation();
   }
 
-  if (pig.position.z > -25) {
+  if (pig.position.z > -40) {
     hasSlipped = true;
   }
 
-  if (pig.position.z > -23) {
+  if (pig.position.z > -35) {
     rotationSpeed += 0.0002;
-    pig.rotation.y -= rotationSpeed;
+    if (doorActions[1].action == "run") {
+      pig.rotation.y -= rotationSpeed;
+    } else {
+      pig.rotation.y += rotationSpeed;
+    }
   }
 
   if (!hasSlipped && !pause) {
-    // Move the pig up and down
     pig.position.y += 0.12 * direction;
     head.position.y += 0.05 * direction;
     eartop.rotation.z += 0.12 * -direction;
@@ -334,7 +324,6 @@ function animatePig() {
 
     pig.rotation.y += 0.001 * 0.5;
 
-    // Reverse direction when reaching a certain height
     if (Math.abs(pig.position.y - initialPig) > 2) {
       direction *= -1;
     }
@@ -357,17 +346,31 @@ function animatePig() {
     }
   }
 
-  if (hasSlipped) {
-    if (pig.position.y > initialPig - 1.5) {
-      pig.position.y -= 0.1;
-    }
-  }
+  if (hasSlipped && pig.position.y > initialPig - 1.5) pig.position.y -= 0.1;
+}
+
+function showPig() {
+  pig.position.set(20, 20, 10);
+  pig.rotation.y = Math.PI / 12;
+
+  head.position.y += 0.015 * direction;
+  [eartop, eartop2].forEach((ear) => (ear.rotation.z += 0.015 * -direction));
+
+  leg_fr.rotation.z += 0.01 * direction;
+  leg_fr.rotation.y += 0.006 * direction;
+
+  if (head.position.y < 1 || head.position.y > 2) direction *= -1;
 }
 
 function resumeAnimation() {
   shouldAnimatePig = false;
-  pig.position.set(150, 20, -105);
-  pig.rotation.y = Math.PI / 12;
+  if (doorActions[1].action == "run") {
+    pig.position.set(150, 20, -105);
+    pig.rotation.y = Math.PI / 12;
+  } else {
+    pig.position.set(-150, 20, -105);
+    pig.rotation.y = Math.PI / 1.2;
+  }
 
   leg_br.rotation.z = Math.PI / 8;
   leg_bl.rotation.z = Math.PI / 8;
@@ -385,9 +388,29 @@ function resumeAnimation() {
 
   rotationSpeed = 0;
   runningSpeed = 0.1;
+  direction = -1;
 
   hasSlipped = false;
   pause = false;
+}
+
+function shuffle() {
+  doorActions[0].action = Math.random() < 0.5 ? "restart" : "run";
+  doorActions[1].action = doorActions[0].action === "run" ? "restart" : "run";
+}
+
+// -------------------- EVENT HANDLERS ---------------------- //
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+window.addEventListener("click", onMouseClick, false);
+
+var playButton = document.getElementById("playButton");
+playButton.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  shouldShowPig = false;
+  resumeAnimation();
+  reposition("introWindow", "36%", "-70%");
 }
 
 function onMouseClick(event) {
@@ -396,31 +419,28 @@ function onMouseClick(event) {
 
   raycaster.setFromCamera(mouse, camera);
 
-  var doors = [door1, door4];
-
-  var intersects = raycaster.intersectObjects(doors);
+  var intersects = raycaster.intersectObjects(
+    doorActions.map((door) => door.door)
+  );
 
   if (intersects.length > 0) {
     var clickedDoor = intersects[0].object;
 
-    if (clickedDoor === door1) {
-      showRestart();
-    } else if (clickedDoor === door4) {
-      shouldAnimatePig = true;
+    const selectedAction = doorActions.find(
+      (door) => door.door === clickedDoor
+    );
+
+    if (selectedAction) {
+      if (selectedAction.action === "restart") {
+        showRestart();
+      } else if (selectedAction.action === "run") {
+        shouldAnimatePig = true;
+      }
     }
   }
 }
 
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-window.addEventListener("click", onMouseClick, false);
-
-animate();
-
-var playButton = document.getElementById("playButton");
-function handleClick(event) {}
-playButton.addEventListener("click", handleClick);
-
+// -------------------- INFORMATION WINDOWS ---------------------- //
 function showGift() {
   var playButton = document.getElementById("playButton");
   playButton.innerText = "RESTART";
@@ -429,6 +449,8 @@ function showGift() {
   introWindow.style.backgroundImage = "url('./images/pig.png')";
 
   reposition("introWindow", "36%", "15%");
+  shuffle(doorActions);
+  shouldShowPig = true;
 }
 
 function showRestart() {
@@ -439,6 +461,9 @@ function showRestart() {
   introWindow.style.backgroundImage = "url('./images/restart.png')";
 
   reposition("introWindow", "36%", "15%");
+  shuffle(doorActions);
+  resumeAnimation();
+  shouldShowPig = true;
 }
 
 function reposition(elementId, left, top) {
@@ -446,3 +471,10 @@ function reposition(elementId, left, top) {
   element.style.left = left;
   element.style.top = top;
 }
+
+// -------------------- START ---------------------- //
+animate();
+
+setTimeout(function () {
+  reposition("introWindow", "36%", "15%");
+}, 200);
